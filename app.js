c69fc230da0c4abf36aa2b68f72620fcc830babf
Nhiyhe Adex <nhiyhe@gmail.com>
2016-05-06 03:13:55 +0100

    middleware setup & ejs

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-/// <reference path="./typings/express/express.d.ts" />
+/// <reference path="./typings/tsd.d.ts" />
 
 var express = require('express'),
+    mongoose = require('mongoose'),
     app     = express();
     
     
+    
+var db = "mongodb://localhost/course_library";
+
+mongoose.connect(db,function(req,res){
+    console.log("Database online...");
+})
+    
 var PORT = process.env.PORT || 9000;
 app.use(express.static(__dirname + ('/public')));
+app.use(express.static(__dirname+('/node_modules/bootstrap/dist')));
+
+app.set('view engine','ejs');
 
 app.get('/',function(req,res){
     res.send('Hello Elibrary');
 });
 
+app.get('/index',function(req,res){
+    res.render('index');
+});
+
 app.listen(PORT,function(req,res){
     console.log("Server is running on port " + PORT);
 })
+
